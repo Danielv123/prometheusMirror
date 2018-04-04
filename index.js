008eaebfe9135f3091bfdc2b6092923c95e85445
Danielv123 <tranen.bananen@gmail.com>
2018-04-04 11:18:06 +0200

    Added basic application

---
+++ index.js
@@ -0,0 +1,42 @@
+const http = require("http");
+
+const queryString = require("query-string");
+const Prometheus = require('prom-client');
+
+const prometheusPrefix = "mirror_";
+Prometheus.collectDefaultMetrics({ timeout: 10000 }); // collects RAM usage etc every 10 s
+
+const gauges = {};
+
+
+http.createServer(function (req, res) {
+	if (req.method == 'POST') {
+		console.log("POST");
+		// req.setHeader("Access-Control-Allow-Origin", "*");
+		var body = '';
+		req.on('data', function (data) {
+			body += data;
+			// console.log("Partial body: " + body);
+		});
+		req.on('end', function () {
+			body = queryString.parse(body);
+			console.log("Body: " + JSON.stringify(body));
+			if(body.name && body.value){
+				if(!Array.isArray(body.labelNames)) body.labelNames = [];
+				if(!gauges[body.name]){
+					gauges[body.name] = new Prometheus.Gauge({
+						name: prometheusPrefix + body.name + "_gauge",
+						help: "Automatically generated value",
+						labelNames: body.labelNames,
+					});
+				}
+				gauges[body.name].inc();
+			}
+		});
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('post received');
+	} else {
+		console.log("GET");
+		res.end(Prometheus.register.metrics());
+	}
+}).listen(8050); //the server object listens on port 8080
